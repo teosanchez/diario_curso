3ba8c5837128ac920cd70e4cb282b5cbf21cf782
Firstname Lastname <teosanchez@gmail.com>
2012-05-28 11:19:32 +0200

    Pruebas del cliente Windows
    Esperemos que funcione

---
+++ inicio.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Diario de clase >> Inicio</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/styles.css" />
+
+        <!-- Scripts -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+    
+
+    </head>
+
+    <body class="portfolio">
+        
+        <!-- Login User -->
+        <?php include ("login_user.php"); ?> 
+        
+        <div id="wrapper" class="container_12 clearfix">
+            <!-- Text Logo -->
+            <h1 id="logo" class="grid_4">Diario de clase</h1>
+
+            <!-- Portfolio Items -->
+            
+            <!-- Section 1 -->
+
+            <!-- Section 3 -->
+            <div class="catagory_1 clearfix">
+                <!-- Row 1 -->
+
+                <div class="grid_12">
+                    <a class="portfolio_item float alpha" href="index.php?cuerpo=rejilla_curso.php">
+                        <span>Administración de Cursos</span>
+                        <img class="" src="images/curso.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float " href="index.php?cuerpo=rejilla_profesor.php">
+                        <span>Administración de Profesores</span>
+                        <img class="" src="images/profesor.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float " href="index.php?cuerpo=rejilla_alumno.php">
+                        <span>Administración de Alumnos</span>
+                        <img class="" src="images/alumno.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float omega" href="portfolio_single.html">
+                        <span>Admin. de Calendarios y Horarios</span>
+                        <img class="" src="images/calendario.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float alpha" href="index.php?cuerpo=menu.php">
+                        <span>Administración General</span>
+                        <img class="" src="images/admin.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float " href="portfolio_single.html">
+                        <span>Diario de clase</span>
+                        <img class="" src="images/diario.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float " href="portfolio_single.html">
+                        <span>Informes y Estadisticas</span>
+                        <img class="" src="images/estadisticas.png"  alt=""/>
+                    </a>
+                    <a class="portfolio_item float omega" href="portfolio_single.html">
+                        <span>Perfil de usuario</span>
+                        <img class="" src="images/conf_cuenta.png"  alt=""/>
+                    </a>              
+                </div>
+            </div>
+
+            <div class="hr grid_12 clearfix">&nbsp;</div>
+
+            <!-- Footer -->
+            <p class="grid_12 footer clearfix">
+                <span class="float"><b>&copy; Copyright</b> Paco & Papita.</span>
+                <a class="float right" href="#">top</a>
+            </p>
+            
+        </div><!--end wrapper-->
+
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,18 @@
+<h2 class="grid_12 caption">   
+    Administración <span>general</span>
+</h2>
+<div class="grid_12">
+    <ul>
+        <li><a href="index.php?cuerpo=rejilla_direccion.php">direccion</a></li>
+        <li><a href="index.php?cuerpo=rejilla_especialidad.php">especialidad</a></li>
+        <li><a href="index.php?cuerpo=rejilla_examen.php">examen</a></li>
+        <li><a href="index.php?cuerpo=rejilla_familia.php">familia</a></li>
+        <li><a href="index.php?cuerpo=rejilla_municipio.php">municipio</a></li>
+        <li><a href="index.php?cuerpo=rejilla_nacionalidad.php">nacionalidad</a></li>
+        <li><a href="index.php?cuerpo=rejilla_nivel_estudios.php">nivel_estudios</a></li>
+        <li><a href="index.php?cuerpo=rejilla_alumno_curso.php">alumno_curso</a></li>
+        <li><a href="index.php?cuerpo=rejilla_profesor_curso.php">profesor_curso</a></li>
+        <li><a href="index.php?cuerpo=rejilla_provincia.php">provincia</a></li> 
+        <li><a href="index.php?cuerpo=rejilla_situacion_laboral.php">sitiacion_laboral</a></li>         
+    </ul>    
+</div>
