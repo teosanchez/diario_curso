5517251afaeaf30de38e71bea6f2af9b91e3cc4b
unknown <maana@anfora-db0a4db5.(none)>
2012-05-29 10:11:30 +0200

    Commit

--- bienvenida.php
+++ bienvenida.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!--Archivo Bienvenida de ERNESTO 1983-->
 
 <div id="main">
@@ -12,3 +13,40 @@
     
 
 </div>
+=======
+
+
+
+<div id="main">
+    <h1>Bienvenido a Diario de clases 1.0</h1>
+
+    <p>Aqui va el mensaje de bienvenida del sistema</p>
+
+    <p>Soy un usuario de tipo  <strong>
+
+        </strong></p>
+
+    <!-- asdasd -->
+
+</div>
+
+
+<!-- Hola Ernesto -->
+
+<!--Archivo Bienvenida de ERNESTO-->
+
+
+<div id="main">
+    <h1>Bienvenido a Diario de clases 1.0</h1>
+
+    <p>Aqui va el mensaje de bienvenida del sistema</p>
+
+    <p>Soy un usuario de tipo  <strong>
+
+        </strong></p>
+
+    
+
+</div>
+
+>>>>>>> branch 'master' of github.com:teosanchez/diario_curso.git
