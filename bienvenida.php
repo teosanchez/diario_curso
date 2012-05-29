c10978f399e58e91db09b7aeb09f5bcc03129e36
Papita <josantonio1989@gmail.com>
2012-05-29 10:10:57 +0200

    papita 10.10

--- bienvenida.php
+++ bienvenida.php
@@ -1,16 +1,8 @@
-<!-- papita prueba -->
-
-
-
-
-
+<!-- bienvenida -->
 <div id="main">
     <h1>Bienvenido a Diario de clases 1.0</h1>
 
     <p>Aqui va el mensaje de bienvenida del sistema</p>
 
-    <p>Soy un usuario de tipo  <strong>
-
-        </strong></p>
-
+    <p>Soy un usuario de tipo  <strong> </strong></p>
 </div>
