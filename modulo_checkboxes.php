f73b7c560c43fa66d01ed157e7cfaafab4155a78
unknown <maana@anfora-74983820.(none)>
2012-05-31 09:00:37 +0200

    modulo-curso

--- modulo_checkboxes.php
+++ modulo_checkboxes.php
@@ -7,13 +7,14 @@ $bd = new bd;
 $util = new utilidadesIU();
 $modulo_curso = new modulo_curso();
 print_r($_GET);
-if (isset($_GET["ID_MODULO_CURSO"])) 
+if (isset($_GET["ID_CURSO"])) 
     {
-    $modulo_curso->ID = ($_GET["ID_MODULO_CURSO"]);
-    }
-if (isset($_GET["ID"])) 
-    {
-    $modulo_curso->ID_CURSO = ($_GET["ID"]);
+        $modulo_curso->ID_CURSO = ($_GET["ID_CURSO"]);
+        $arrayEntidad = $bd->buscar($modulo_curso);
+        if ($arrayEntidad) 
+        {
+            $modulo_curso->cargar($arrayEntidad[0]);
+        }
     }
 
 ?>
@@ -21,7 +22,8 @@ if (isset($_GET["ID"]))
 
     <div class="titulo">
         <div class="grid_9 alpha"">
-             <h2 class="caption">Edici&oacute;n M&oacute;dulo <span>Chekboxes</span></h2>
+             <h2 class="caption">Edici&oacute;n M&oacute;dulos 
+                <span>ID_CURSO = <?php echo($modulo_curso->ID_CURSO); ?></span></h2>
         </div>
         <div class="grid_3 omega">            
         </div> 
@@ -30,11 +32,11 @@ if (isset($_GET["ID"]))
     <!-- Fin Titulo de pÃ¡gina -->
 
     <form name="form_modulo_checkboxes" method="get" action="procesar_modulo_checkboxes.php"/>
-        <input type="hidden" name="ID_MODULO_CURSO" id="ID_MODULO_CURSO" value="<?php echo $modulo_curso->ID; ?>"/>
+        <input type="hidden" name="ID_CURSO" id="ID_CURSO" value="<?php echo $modulo_curso->ID_CURSO; ?>"/>
         <input type="hidden" name="ID" id="ID" value="<?php echo $modulo_curso->ID_CURSO; ?>"/>
         <table>
             <tr>
-                <td>M&oacute;dulos</td>
+                <!--<td>M&oacute;dulos</td>-->
                 <td>
                     <?php
                     $modulos = $bd->consultarArray("SELECT * FROM modulo ORDER BY NOMBRE asc");
--- procesar_modulo_checkboxes.php
+++ procesar_modulo_checkboxes.php
@@ -7,39 +7,35 @@ $bd = new bd();
 $modulo_curso = new modulo_curso();
 if (isset($_GET["Enviar"])) 
 {
-    if (isset($_GET["ID"])) 
+    if (isset($_GET["ID_CURSO"])) 
     {
-        $modulo_curso->ID_CURSO = $_GET["ID"];
-        $arrayEntidad = $bd->buscar($modulo_curso);
-        if ($arrayEntidad) 
+        $modulo_curso->ID_CURSO = $_GET["ID_CURSO"];
+        $bd->consultar("delete from modulo_curso where ID_CURSO='" . $modulo_curso->ID_CURSO . "'");
+
+        if (isset($_GET["Modulos_Seleccionados"])) 
         {
-            //$modulo_curso->cargar($arrayEntidad[0]);
-            $bd->consultar("delete from modulo_curso where ID_CURSO='" . $modulo_curso->ID_CURSO . "'");
-            //$msj2 = "Registro Eliminado Correctamente."; //Incluir en Generador
+            $Modulos_Seleccionados = $_GET["Modulos_Seleccionados"];
 
-            if (isset($_GET["Modulos_Seleccionados"])) 
+            $num_modulos = count($Modulos_Seleccionados);
+            echo ("num_modulos: " . $num_modulos);
+            for ($i = 0; $i < $num_modulos; $i++) 
             {
-                $Modulos_Seleccionados = $_GET["Modulos_Seleccionados"];
-
-                $num_modulos = count($Modulos_Seleccionados);
-                echo ("num_modulos: " . $num_modulos);
-                for ($i = 0; $i < $num_modulos; $i++) 
-                {
-                    $modulo_curso->ID_MODULO = $Modulos_Seleccionados[$i];
-                    $bd->insertar($modulo_curso);
-                    //$msj2 = "Registro Insertado Correctamete."; //Incluir en Generador
-                }
+                $modulo_curso->ID_MODULO = $Modulos_Seleccionados[$i];
+                $bd->insertar($modulo_curso);
+                //$msj2 = "Registro Insertado Correctamete."; //Incluir en Generador
             }
         }
-        header('Location: index.php?cuerpo=form_modulo_curso.php&ID='.$modulo_curso->ID); //Incluir en Generador
+        
+        header('Location: index.php?cuerpo=rejilla_modulo_curso.php&ID='.$modulo_curso->ID_CURSO); //Incluir en Generador
     }
 }
 if (isset($_GET["Cancelar"]))
 {
-    if (isset($_GET["ID_MODULO_CURSO"])) 
+    
+    if (isset($_GET["ID_CURSO"])) 
     {
-        $modulo_curso->ID = $_GET["ID_MODULO_CURSO"];
-        header('Location: index.php?cuerpo=form_modulo_curso.php&ID='.$modulo_curso->ID); //Incluir en Generador
+        $modulo_curso->ID_CURSO = $_GET["ID_CURSO"];
+        header('Location: index.php?cuerpo=rejilla_modulo_curso.php&ID='.$modulo_curso->ID_CURSO); //Incluir en Generador
     }
 }
    
